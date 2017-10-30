aca9a1aae5ce58079bb12baf616a95abd68c963f
Qinglin <316032931@qq.com>
2017-10-30 17:34:41 +0800

    v1.0

--- src/Count.java
+++ src/Count.java
@@ -4,10 +4,16 @@ import java.util.*;
 
 import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
+
+/**
+ * 同一个ip的历史下载总次数统计
+ */
 public class Count {
     public static void main(String[] args) throws Exception{
         String tempIP;
-        String[] filePathOlds = new String[] {"logs/log-0912-19.csv", "logs/log-0920-26.csv"};
+        //数组中为待统计的所有往期原始文件（log-****-**.csv）
+        String[] filePathOlds = new String[] {"logs/log-0912-19.csv", "logs/log-0920-26.csv", "logs/log-0927-1003.csv", "logs/log-1004-1008.csv"
+        , "logs/log-1009-1013.csv", "logs/log-1020-1026.csv"};
         Map<String, String> records = new HashMap<String, String>();
         CsvReader csvreader_old;
         for(String filePathOld : filePathOlds) {
@@ -21,8 +27,8 @@ public class Count {
                 }
             }
         }
-
-        String filePathOut = "count/count-till0926.csv";
+        //输出为截止到本期位置的完整历史统计结果
+        String filePathOut = "count/count-till1026.csv";
             int count = 0;
             CsvWriter csvwriter = new CsvWriter(filePathOut);
             csvwriter.writeRecord(new String[]{"IP","count"});
--- src/LogClean.java
+++ src/LogClean.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
+
+/**
+ * 格式说明：
+ *     log-0912-19.csv为原始文件
+ * out-log-0912-19.csv为对应的LogClean的输出文件（没有删除重复ip（全局不重复））
+ */
 public class LogClean {
     public static void main(String[] args) throws Exception{
         String tempIP;
         List<String> ls = new ArrayList<String>();
-        String[] filePathOlds = new String[] {"logs/out-log-0912-19.csv"};
+        //filePathOlds为往期的输出文件数组，举例：
+        //当清洗第三期的原始文件（log-0927-1003.csv）时，该数组中为第一第二期的输出文件（out-log-0912-19.csv, out-log-0920-26.csv）
+        String[] filePathOlds = new String[] {"logs/out-log-0912-19.csv", "logs/out-log-0920-26.csv", "logs/out-log-0927-1003.csv"
+        , "logs/out-log-1004-1008.csv", "logs/out-log-1009-1013.csv"};
         CsvReader csvreader_old;
         for(String filePathOld : filePathOlds) {
             csvreader_old = new CsvReader(filePathOld);
@@ -19,9 +28,10 @@ public class LogClean {
         }
 //        String filePathOld = "out-test1.csv";
 
-
-        String filePathIn = "logs/log-0920-26.csv";
-        String filePathOut = "logs/out-log-0920-26.csv";
+        //当前要清洗的原始文件
+        String filePathIn = "logs/log-1020-1026.csv";
+        //当前文件清洗后的输出文件
+        String filePathOut = "logs/out-log-1020-1026.csv";
         CsvReader csvreader = new CsvReader(filePathIn);
         if(csvreader.readHeaders()) {
             int count = 0;
